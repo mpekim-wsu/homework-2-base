bfd613bebcdfd135a0dc2103d05f70027683d788
mpekim_wsu <mmorley@worcester.edu>
2021-11-16 15:50:43 -0500

    Fixing the file. Hopefully this will allow the rebase to continue.

--- hello.rb
+++ hello.rb
@@ -1,4 +1,8 @@
+
 # This an unwanted but committed comment. Checkpoint Lab 17.
+
+# Default is "World"
+# Author: Mike Morley (mmorley@worcester.edu)
 name = ARGV.first || "World"
 
 puts "Hello, #{name}!"
